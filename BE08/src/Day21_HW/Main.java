e5967c9f24e3c5bf38037c2d335432e2453275b0
jonathandao-au <jonathandao.au@gmail.com>
2025-06-21 02:13:03 +0930

    Updates for DFS with Recursion.

--- BE08/src/Day21_HW/Main.java
+++ BE08/src/Day21_HW/Main.java
@@ -5,30 +5,36 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		BE08Tree tree = new BE08Tree(null);
-		BE08Node root = new BE08Node(null);
+		BE08Node root = new BE08Node(1);
 		root.value = 1;
 
 		tree.root = root;
-		BE08Node node = new BE08Node(null);
+		BE08Node node = new BE08Node(1);
 		node.value = 30;
 		tree.root.children.add(node);
-		
-		node = new BE08Node(null);
+
+		node = new BE08Node(1);
 		node.value = 40;
 		tree.root.children.add(node);
 
-		node = new BE08Node(null);
+		node = new BE08Node(1);
 		node.value = 20;
 		BE08Node findNode = tree.nodeSearchBFS(30);
 		findNode.children.add(node);
-		
-		node = new BE08Node(null);
+
+		node = new BE08Node(1);
 		node.value = 10;
 		findNode = tree.nodeSearchDFS(30);
 		findNode.children.add(node);
 
+		node = new BE08Node(1);
+		node.value = 60;
+		findNode = tree.nodeSearchDFS(10);
+		findNode.children.add(node);
+
 		System.out.println("1. BFS Search");
 		System.out.println("2. DFS Search");
+		System.out.println("3. DFS Search with Recursive");
 
 		Scanner scanner = new Scanner(System.in);
 
@@ -53,6 +59,15 @@ public class Main {
 				System.out.println("DFS Search for " + search + " : Found " + resultDFS.value + ".");
 			} else {
 				System.out.println("DFS Search for " + search + " : Not found.");
+			}
+		} else if (choice == 3) {
+			System.out.print("Search for: ");
+			int search = scanner.nextInt();
+			BE08Node resultDFS = tree.nodeSearchDFSRecursive(search);
+			if (resultDFS != null) {
+				System.out.println("DFS Search with Recursion for " + search + " : Found " + resultDFS.value + ".");
+			} else {
+				System.out.println("DFS Search with Recursion for " + search + " : Not found.");
 			}
 		} else {
 			System.out.println("Invalid option.");
