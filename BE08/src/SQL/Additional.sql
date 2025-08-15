33b04a4d47509be6b4154397ae0170e46c25a2e5
jonathandao-au <jonathandao.au@gmail.com>
2025-08-15 20:11:40 +0930

    Update Additional.sql

--- BE08/src/SQL/Additional.sql
+++ BE08/src/SQL/Additional.sql
@@ -26,6 +26,7 @@ WHERE name LIKE '%a%';
 -- COUNT(*) counts rows. WHERE filters by country_code.
 -- COUNT(*) counts null rows as well whereas others don't.
 -- COUNT(*) is O(1) while COUNT(attribute) is O(n). (*) is just array.length while (attribute) is a null-checking loop.
+-- Use COUNT(*) when data within is not a concern.
 SELECT COUNT(*) AS total_us_films
 FROM film
 WHERE country_code = 'US';
